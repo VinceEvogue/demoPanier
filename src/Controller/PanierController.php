0b136865f13a4d69cc9df36815fcc6d84dfb4679
unknown <imthelaw115@gmail.com>
2023-08-29 10:40:43 +0200

    fin panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -13,38 +13,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PanierController extends AbstractController
 {
     #[Route('/cart', name:'cart')]
-    public function index(RequestStack $rs, ProductRepository $repo)
+    public function index(CartService $cs)
     {
-
-        $session = $rs->getSession();
-        $cart = $session->get('cart', []);
-
-        //Je vais créer un nouveau tableau qui contiendra des objets Product et les quantité de chaque objet
-        $cartWithData = [];
-
-        //Pour chaque id qui se trouve dans le tableau $cart, on ajoute une case(tableau) dans cartWithData, qui est un tableau multidimensionnel
-        foreach($cart as $id => $quantity)
-        {
-            $cartWithData[] = [
-                'product' => $repo->find($id),
-                'quantity' => $quantity,
-                
-            ];
-        }
-
-        $total = 0; // j'initialise mon total
-
-        foreach($cartWithData as $item)
-        {
-            $sousTotal = $item['product']->getPrice() * $item['quantity'];
-            $total += $sousTotal;
-
-
-        }
-
+   
         return $this->render('panier/index.html.twig', [
-            'items' => $cartWithData,
-            'total' => $total
+            'items' => $cs->getCartWithData(),
+            'total' => $cs->getTotal()
         ]);
 
     }
@@ -57,28 +31,9 @@ class PanierController extends AbstractController
     }
 
     #[Route('/cart/remove/{id}', name:'cart_remove')]
-    public function remove($id, RequestStack $rs)
+    public function remove($id, CartService $cs)
     {
-        $session = $rs->getSession();
-        $cart = $session->get('cart', []);
-        $qt = $session->get('qt', 0);
-        //! si l'id existe dans mon panier, je le supprime du tableau grace a unset()
-        if(!empty($cart[$id]))
-        {
-            $qt -= $cart[$id];
-            unset($cart[$id]);
-        }
-
-        //gérer l'erreur possible négative.
-        if($qt < 0 )
-        {
-            $qt = 0;
-        }
-
-        $session->set('qt', $qt);
-        $session->set('cart', $cart);
-
-
+        $cs->remove($id);
         return $this->redirectToRoute('cart');
     }
 }
--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -45,4 +45,62 @@ class CartService
         $session->set('cart', $cart);
         $session->set('qt', $qt);
     }
+
+    public function remove($id)
+    {
+        $session = $this->rs->getSession();
+        $cart = $session->get('cart', []);
+        $qt = $session->get('qt', 0);
+        //! si l'id existe dans mon panier, je le supprime du tableau grace a unset()
+        if(!empty($cart[$id]))
+        {
+            $qt -= $cart[$id];
+            unset($cart[$id]);
+        }
+
+        //gérer l'erreur possible négative.
+        if($qt < 0 )
+        {
+            $qt = 0;
+        }
+
+        $session->set('qt', $qt);
+        $session->set('cart', $cart);
+    }
+
+    public function getCartWithData()
+    {
+        $session = $this->rs->getSession();
+        $cart = $session->get('cart', []);
+
+        //Je vais créer un nouveau tableau qui contiendra des objets Product et les quantité de chaque objet
+        $cartWithData = [];
+
+        //Pour chaque id qui se trouve dans le tableau $cart, on ajoute une case(tableau) dans cartWithData, qui est un tableau multidimensionnel
+        foreach($cart as $id => $quantity)
+        {
+            $cartWithData[] = [
+                'product' => $this->repo->find($id),
+                'quantity' => $quantity,
+                
+            ];
+        }
+
+        return $cartWithData;
+    }
+
+    public function getTotal()
+    {
+        $total = 0; // j'initialise mon total
+
+        foreach($this->getCartWithData() as $item)
+        {
+            $sousTotal = $item['product']->getPrice() * $item['quantity'];
+            $total += $sousTotal;
+        }
+
+        return $total;
+
+    }
+
 }
